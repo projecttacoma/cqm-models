8a139cd6dd41ff9550d812e33bb7809c13fe59b6
Michael O'Keefe <mokeefe@mitre.org>
2018-03-26 16:14:50 -0400

    Added comments to Measure objects

--- app/assets/javascripts/Measure.js
+++ app/assets/javascripts/Measure.js
@@ -3,8 +3,8 @@ const Code = require('./basetypes/Code');
 const Interval = require('./basetypes/Interval');
 const Quantity = require('./basetypes/Quantity');
 
-const [Integer, Array, String, Boolean, Mixed, ObjectId, Date] = [
-  mongoose.Schema.Types.Integer,
+const [Number, Array, String, Boolean, Mixed, ObjectId, Date] = [
+  mongoose.Schema.Types.Number,
   mongoose.Schema.Types.Array,
   mongoose.Schema.Types.String,
   mongoose.Schema.Types.Boolean,
@@ -15,34 +15,36 @@ const [Integer, Array, String, Boolean, Mixed, ObjectId, Date] = [
 
 const MeasureSchema = mongoose.Schema(
   {
+    // ID/other measure information
     id: String,
     measure_id: String,
     hqmf_id: String,
     hqmf_set_id: String,
-    hqmf_version_number: Integer,
+    hqmf_version_number: Number,
     cms_id: String,
     title: String,
     description: String,
     type: String,
     category: { type: String, default: 'Uncategorized' },
 
+    // Measure type variables
     episode_of_care: Boolean,
     continuous_constiable: Boolean,
-    episode_ids: [], // not sure if this is necessary
+    episode_ids: [],
 
-    needs_finalize: Boolean, // Bonnie-specific?
-
+    // Publishing data (used by Bonnie)
     published: Boolean,
     publish_date: Date,
-    version: Integer,
+    version: Number,
 
+    // ELM/CQL Measure-logic related data
     elm_annotations: Mixed,
-
     cql: [String],
     elm: [Mixed],
     main_cql_library: String,
     cql_statement_dependencies: Mixed,
 
+    // HQMF/Tacoma-specific Measure-logic related data
     population_criteria: Mixed,
     data_criteria: Mixed,
     source_data_criteria: Mixed,
@@ -56,6 +58,7 @@ const MeasureSchema = mongoose.Schema(
 
     complexity: Mixed, // Bonnie-specific?
 
+    // Relations to other model classes
     user: { type: ObjectId, ref: 'User', index: true }, // Bonnie-specific
     bundle: { type: ObjectId, ref: 'Bundle' }, // Cypress-specific, but used in Bonnie-bundler as well
     package: { type: ObjectId, ref: 'MeasurePackage' }, // Bonnie specific
