881fb42f4d1161ce7489b3b832f5b03583d9d17d
Michael O'Keefe <mokeefe@mitre.org>
2018-03-26 19:12:15 -0400

    Made rubocop-requested changes, and disabled a cop for some ported code

--- app/models/qdm/tacoma/concept.rb
+++ app/models/qdm/tacoma/concept.rb
@@ -1,13 +1,14 @@
 module QDM
+  # Concept is an individual code within a ValueSet, including all of the associated metadata
+  # such as code system, version, and display name
   class Concept
     include Mongoid::Document
-    include Mongoid::Attributes::Dynamic
     field :code, type: String
     field :code_system_name, type: String
     field :code_system_version, type: String
     field :display_name, type: String
     field :code_system, type: String
-    scope :by_code_system_name,  ->(cs){where(code_system_name: cs)}
-    scope :by_code_system,  ->(cs){where(code_system: cs)}
+    scope :by_code_system_name, ->(cs) { where(code_system_name: cs) }
+    scope :by_code_system, ->(cs) { where(code_system: cs) }
   end
 end
--- app/models/qdm/tacoma/measure_package.rb
+++ app/models/qdm/tacoma/measure_package.rb
@@ -1,7 +1,10 @@
-class MeasurePackage
-  include Mongoid::Document
-  include Mongoid::Timestamps
+module QDM
+  # MeasurePackage stores the uploaded file for a given Measure, so it can be recreated if need be
+  class MeasurePackage
+    include Mongoid::Document
+    include Mongoid::Timestamps
 
-  field :file, type: BSON::Binary
-  belongs_to :measure, class_name: "Measure", inverse_of: :package
+    field :file, type: BSON::Binary
+    belongs_to :measure, class_name: 'Measure', inverse_of: :package
+  end
 end
