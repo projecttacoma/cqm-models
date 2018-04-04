ef92cec36a9e3064722b4f5a2d824fc2a90d34b8
Louis Ades <lades@mitre.org>
2018-04-04 15:28:37 -0400

    Refactored js basetypes to fit the format for schematypes

--- app/assets/javascripts/basetypes/Code.js
+++ app/assets/javascripts/basetypes/Code.js
@@ -1,30 +1,25 @@
 const mongoose = require('mongoose');
 
-class Code {
-  constructor(code, code_system, descriptor, code_system_oid, version) {
-    this.code = code;
-    this.code_system = code_system;
-    this.descriptor = descriptor;
-    this.code_system_oid = code_system_oid;
-    this.version = version;
-  }
-
-  toBSON() {
-    const code = {};
-    code.code = this.code;
-    code.code_system = this.code_system;
-    code.descriptor = this.descriptor;
-    code.code_system_oid = this.code_system_oid;
-    code.version = this.version;
-    return code;
-  }
+function Code(key, options) {
+  mongoose.SchemaType.call(this, key, options, 'Code');
 }
+Code.prototype = Object.create(mongoose.SchemaType.prototype);
 
-class CodeSchema extends mongoose.SchemaType {
-  static cast(code) {
-    return new Code(code.code, code.code_system, code.descriptor, code.code_system_oid, code.version);
+Code.prototype.cast = (code) => {
+  if (typeof code.code === 'undefined') {
+    throw new Error(`Code: ${code} does not have a code`);
+  } else if (typeof code.code_system === 'undefined') {
+    throw new Error(`Code: ${code} does not have a code_system`);
   }
-}
 
-mongoose.Schema.Types.Code = CodeSchema;
+  const val = { code: code.code, code_system: code.code_system };
+
+  val.descriptor = (typeof code.descriptor !== 'undefined') ? code.descriptor : null;
+  val.code_system_oid = (typeof code.code_system_oid !== 'undefined') ? code.code_system_oid : null;
+  val.version = (typeof code.version !== 'undefined') ? code.version : null;
+
+  return val;
+};
+
+mongoose.Schema.Types.Code = Code;
 module.exports = Code;
