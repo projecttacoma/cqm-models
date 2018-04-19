8633fbd4e2f8577e0315219e9def66119d93f1fd
Michael O'Keefe <mokeefe@mitre.org>
2018-04-19 12:43:25 -0400

    Implement a validate_dist shell script, and add to travis

---
+++ bin/validate_dist.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+mkdir -p tmp/dist/
+
+# dist_test browserifies index.js to tmp/dist
+yarn run dist_test
+
+# comm -3 only returns lines that differ between the two files. If none are different, diff will be empty
+diff=`comm -3 dist/index.js tmp/dist/index.js`
+
+# Exit with a non-zero code if the diff isn't empty
+if [ diff != "" ]; then
+  exit 1
+fi
