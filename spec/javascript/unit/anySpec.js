96189c585512a8a04d317c3d1a7a42936953cae5
Louis Ades <lades@mitre.org>
2018-04-20 10:54:13 -0400

    Add test cases for Any object casting

--- spec/javascript/unit/anySpec.js
+++ spec/javascript/unit/anySpec.js
@@ -14,5 +14,49 @@ describe('The Any class', () => {
       expect(returned_obj.value).toEqual(2);
       expect(returned_obj.unit).toEqual('m');
     });
+
+    it('Should convert a Interval JS Object to its type', () => {
+      const interval_obj = {
+        low: '2012-05-21T11:30:00.000-04:00',
+      };
+
+      const returned_obj = Any.prototype.cast(interval_obj);
+      expect(returned_obj instanceof Cql.Interval).toBe(true);
+      expect(returned_obj.low instanceof Cql.DateTime).toEqual(true);
+    });
+
+    it('Should convert a DateTime JS Object to its type', () => {
+      const datetime_obj = '2012-05-21T11:30:00.000-04:00';
+
+      const returned_obj = Any.prototype.cast(datetime_obj);
+      expect(returned_obj instanceof Cql.DateTime).toBe(true);
+    });
+
+    it('Should convert a non-special JS Object to its type', () => {
+      const any_obj = { hi: 'no' };
+
+      const returned_obj = Any.prototype.cast(any_obj);
+      expect(returned_obj instanceof Object).toEqual(true);
+      expect(returned_obj.hi).toEqual('no');
+    });
+
+    it('Should recursively convert an Array JS Object to its type', () => {
+      const array_obj = [{
+        low: '2012-05-21T11:30:00.000-04:00',
+      }, {
+        value: 2,
+        unit: 'm',
+      }, {
+        hi: 'no',
+      }];
+
+      const returned_obj = Any.prototype.cast(array_obj);
+      expect(Array.isArray(returned_obj)).toBe(true);
+      expect(returned_obj[0] instanceof Cql.Interval).toEqual(true);
+      expect(returned_obj[0].low instanceof Cql.DateTime).toEqual(true);
+      expect(returned_obj[1] instanceof Cql.Quantity).toEqual(true);
+      expect(returned_obj[2] instanceof Object).toEqual(true);
+      expect(returned_obj[2].hi).toEqual('no');
+    });
   });
 });
