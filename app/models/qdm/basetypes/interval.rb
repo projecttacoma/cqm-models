024f6158416665207ba11c0986a6ea842435b881
lmd59 <lmd59@cornell.edu>
2018-06-13 11:14:55 -0400

    Shift dates (#36)
    * Experimental shift dates functionality for patient data elements
    
    * expand to include data elements and birthDatetime
    
    * some test updates
    
    * self
    
    * adjust datetime type for interval lows and highs
    
    * check for Time type as well

--- app/models/qdm/basetypes/interval.rb
+++ app/models/qdm/basetypes/interval.rb
@@ -16,6 +16,21 @@ module QDM
       { low: @low, high: @high, lowClosed: @lowClosed, highClosed: @highClosed }
     end
 
+    # Shift dates by the given value.
+    # Given value should be in seconds. Positive values shift forward, negative
+    # values shift backwards.
+    #
+    # NOTE: This will only shift if @high and @low are DateTimes.
+    def shift_dates(seconds)
+      if (@low.is_a? DateTime) || (@low.is_a? Time)
+        @low = (@low.utc.to_time + seconds.seconds).to_datetime.new_offset(0)
+      end
+      if (@high.is_a? DateTime) || (@high.is_a? Time)
+        @high = (@high.utc.to_time + seconds.seconds).to_datetime.new_offset(0)
+      end
+      self
+    end
+
     class << self
       # Get the object as it was stored in the database, and instantiate
       # this custom class from it.
@@ -25,6 +40,7 @@ module QDM
       def demongoize(object)
         return nil unless object
         object = object.symbolize_keys
+        fix_datetime(object)
         QDM::Interval.new(object[:low], object[:high], object[:lowClosed], object[:highClosed]) if object.is_a?(Hash)
       end
 
@@ -36,9 +52,16 @@ module QDM
         when QDM::Interval then object.mongoize
         when Hash
           object = object.symbolize_keys
+          fix_datetime(object)
           QDM::Interval.new(object[:low], object[:high], object[:lowClosed], object[:highClosed]).mongoize
         else object
         end
+      end
+
+      def fix_datetime(object)
+        # Cast to DateTime if it is a string representing a DateTime
+        object[:low] = DateTime.strptime(object[:low]) if (object[:low].is_a? String) && DateTime.strptime(object[:low])
+        object[:high] = DateTime.strptime(object[:high]) if (object[:high].is_a? String) && DateTime.strptime(object[:high])
       end
 
       # Converts the object that was supplied to a criteria and converts it
