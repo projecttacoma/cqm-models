6f2852ea1b8460bebdf4b77031002b23beb7a3ad
Adam M. Holmes <aholmes@mitre.org>
2018-07-05 10:55:27 -0400

    Update DataElement.js

--- app/assets/javascripts/basetypes/DataElement.js
+++ app/assets/javascripts/basetypes/DataElement.js
@@ -14,14 +14,25 @@ function DataElementSchema(add, options) {
     extended.add(add);
   }
 
-  // Returns all of the codes on this data element
-  // in a format usable by the cql-execution framework
+  // Returns all of the codes on this data element in a format usable by
+  // the cql-execution framework.
   extended.methods.getCode = function getCode() {
     return this.dataElementCodes.map((code) => {
       return new cql.Code(code.code, code.codeSystem, code.version, code.descriptor);
     });
   };
 
+  // Return the first code on this data element in a format usable by
+  // the cql-execution framework.
+  extended.methods.code = function code() {
+    if (this.dataElementCodes && this.dataElementCodes[0]) {
+      const qdmCode = this.dataElementCodes[0];
+      return new cql.Code(qdmCode.code, qdmCode.codeSystem, qdmCode.version, qdmCode.descriptor);
+    } else {
+      return null;
+    }
+  };
+  
   // Returns all of the codes on this data element
   // in their normal form.
   extended.methods.codes = function getCodes() {
