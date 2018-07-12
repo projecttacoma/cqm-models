f7253f4b94a98fddaa619b34663a5d0518721e39
Matt Mayer <mmayer@mitre.org>
2018-07-12 07:57:08 -0400

    Remove cast function from Code.js, code cast is now performed in Any.js

--- app/assets/javascripts/basetypes/Code.js
+++ app/assets/javascripts/basetypes/Code.js
@@ -5,21 +5,5 @@ function Code(key, options) {
 }
 Code.prototype = Object.create(mongoose.SchemaType.prototype);
 
-Code.prototype.cast = (code) => {
-  if (typeof code.code === 'undefined') {
-    throw new Error(`Code: ${code} does not have a code`);
-  } else if (typeof code.codeSystem === 'undefined') {
-    throw new Error(`Code: ${code} does not have a codeSystem`);
-  }
-
-  const val = { code: code.code, codeSystem: code.codeSystem };
-
-  val.descriptor = (typeof code.descriptor !== 'undefined') ? code.descriptor : null;
-  val.codeSystemOid = (typeof code.codeSystemOid !== 'undefined') ? code.codeSystemOid : null;
-  val.version = (typeof code.version !== 'undefined') ? code.version : null;
-
-  return val;
-};
-
 mongoose.Schema.Types.Code = Code;
 module.exports = Code;
