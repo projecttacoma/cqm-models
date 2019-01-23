138cdb5c3c220ea58a550d82f0ce327ecc1cf3b9
Luke Osborne <lwosborne@mitre.org>
2019-01-23 12:46:55 -0500

    Suppress simplecov output for non-CI ruby tests

--- spec/simplecov_init.rb
+++ spec/simplecov_init.rb
@@ -1,8 +1,28 @@
 require 'simplecov'
 require 'codecov'
 
+require 'simplecov'
+
 SimpleCov.start do
   add_filter 'spec/'
 end
 
-SimpleCov.formatter = SimpleCov::Formatter::Codecov
+module SimpleCov
+  module Formatter
+    class QualityFormatter
+      def format(result)
+        SimpleCov::Formatter::HTMLFormatter.new.format(result)
+        File.open('coverage/covered_percent', 'w') do |f|
+          f.puts result.source_files.covered_percent.to_f
+        end
+      end
+    end
+  end
+end
+
+if ENV['CI'] == 'true'
+  require 'codecov'
+  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+else
+  SimpleCov.formatter = SimpleCov::Formatter::QualityFormatter
+end
