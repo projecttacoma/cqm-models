57cebe0d8aee7a9fc14fa55b9d3946517a41ad4c
David Czulada <dczulada@mitre.org>
2019-01-29 12:15:04 -0500

    Add provider to cqm patient model

--- app/models/cqm/provider_performance.rb
+++ app/models/cqm/provider_performance.rb
@@ -1,13 +1,17 @@
-class ProviderPerformance
-  include Mongoid::Document
-  
-  field :start_date, type: Integer
-  field :end_date, type: Integer
-  
-  belongs_to :provider
+module CQM
+  class ProviderPerformance
+    include Mongoid::Document
+    
+    embedded_in :patient
 
-  def shift_dates(date_diff)
-    self.start_date = self.start_date.nil? ? nil : self.start_date + date_diff
-    self.end_date = self.end_date.nil? ? nil : self.end_date + date_diff
+    field :start_date, type: Integer
+    field :end_date, type: Integer
+    
+    belongs_to :provider
+
+    def shift_dates(date_diff)
+      self.start_date = self.start_date.nil? ? nil : self.start_date + date_diff
+      self.end_date = self.end_date.nil? ? nil : self.end_date + date_diff
+    end
   end
 end
