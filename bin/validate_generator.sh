b6b2b2626eda5a870a8611ccd33fb78b3479357d
Luke Osborne <lwosborne@mitre.org>
2019-02-15 08:26:55 -0500

    return -> exit

--- bin/validate_generator.sh
+++ bin/validate_generator.sh
@@ -4,9 +4,9 @@ yes | ruby lib/generate_models.rb modelinfo/qdm-modelinfo-5.4.xml data/oids.json
 
 if git diff-index --quiet HEAD --; then
   echo "Generator script produces no differences."
-  return 0
+  exit 0
 else
   echo "Differences found when running generator script!  Make sure there are no
   unstaged or untracked files."
-  return 1
+  exit 1
 fi
