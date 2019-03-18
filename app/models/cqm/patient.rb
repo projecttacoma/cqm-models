f07e50250a4ee66812c8384177b4013152e03832
Luke Osborne <lwosborne@mitre.org>
2019-03-18 12:30:14 -0400

    add has_and_belongs_to_many :measure to cqm patient model

--- app/models/cqm/patient.rb
+++ app/models/cqm/patient.rb
@@ -8,6 +8,7 @@ module CQM
     field :expectedValues, type: Array
     field :notes, type: String
     embeds_one :qdmPatient, class_name: 'QDM::Patient', autobuild: true
+    has_and_belongs_to_many :measures, class_name: 'CQM::Measure'
 
     # Include '_type' in any JSON output. This is necessary for deserialization.
     def to_json(options = nil)
