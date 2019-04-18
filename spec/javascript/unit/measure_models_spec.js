74647bdb951cac58ecba9207a1a899b9bf36fd0f
Matt Mayer <mayerm94@gmail.com>
2019-04-18 07:45:49 -0400

    Initialize SDC as concrete types on measure (#98)
    * Initialize SDC as concrete types on measure
    * Add test for SDC parsing

--- spec/javascript/unit/measure_models_spec.js
+++ spec/javascript/unit/measure_models_spec.js
@@ -1,4 +1,5 @@
 const CQM = require('./../../../app/assets/javascripts/cqm/AllCQMModels')
+const AdverseEvent = require('./../../../app/assets/javascripts/AdverseEvent.js').AdverseEvent;
 const Buffer = require('mongoose/browser').Schema.Types.Buffer;
 
 describe('CQM', () => {
@@ -11,11 +12,17 @@ describe('CQM', () => {
         hqmf_id: '40280582-5C5B-CF39-015C-A69BB10D15D8',
         hqmf_set_id: '6DB434E0-8FC7-42C8-B3B2-A7653E2A0D16',
         hqmf_version_number: '0.1.023',
+        source_data_criteria: [
+          new AdverseEvent(),
+          new AdverseEvent(),
+          new AdverseEvent()
+        ],
         cms_id: 'CMS9999v3',
         main_cql_library: 'TestMainLibrary'
       });
       err = measure.validateSync();
       expect(err).toBeUndefined();
+      expect(measure.source_data_criteria.length).toEqual(3)
     });
 
     it('can construct and save a measure with two population_sets', () => {
