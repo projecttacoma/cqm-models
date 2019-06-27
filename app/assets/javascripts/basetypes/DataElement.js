ce721d5ed2ec8c92a5c291bb11393c37a46c02fd
Matt Mayer <mmayer@mitre.org>
2019-06-27 09:14:01 -0400

    Shortcut code creation

--- app/assets/javascripts/basetypes/DataElement.js
+++ app/assets/javascripts/basetypes/DataElement.js
@@ -25,7 +25,12 @@ function DataElementSchema(add, options) {
   // Returns all of the codes on this data element in a format usable by
   // the cql-execution framework.
   extended.methods.getCode = function getCode() {
-    return this.dataElementCodes.map(code => new cql.Code(code.code, code.system, code.version, code.display));
+    return this.dataElementCodes.map((code) => {
+      if (code.isCode) {
+        return code;
+      }
+      return new cql.Code(code.code, code.system, code.version, code.display);
+    });
   };
 
   // Return the first code on this data element in a format usable by
@@ -33,6 +38,9 @@ function DataElementSchema(add, options) {
   extended.methods.code = function code() {
     if (this.dataElementCodes && this.dataElementCodes[0]) {
       const qdmCode = this.dataElementCodes[0];
+      if (qdmCode.isCode) {
+        return qdmCode;
+      }
       return new cql.Code(qdmCode.code, qdmCode.system, qdmCode.version, qdmCode.display);
     }
     return null;
--- dist/browser.js
+++ dist/browser.js
@@ -2911,7 +2911,12 @@ function DataElementSchema(add, options) {
   // Returns all of the codes on this data element in a format usable by
   // the cql-execution framework.
   extended.methods.getCode = function getCode() {
-    return this.dataElementCodes.map(code => new cql.Code(code.code, code.system, code.version, code.display));
+    return this.dataElementCodes.map((code) => {
+      if (code.isCode) {
+        return code;
+      }
+      return new cql.Code(code.code, code.system, code.version, code.display);
+    });
   };
 
   // Return the first code on this data element in a format usable by
@@ -2919,6 +2924,9 @@ function DataElementSchema(add, options) {
   extended.methods.code = function code() {
     if (this.dataElementCodes && this.dataElementCodes[0]) {
       const qdmCode = this.dataElementCodes[0];
+      if (qdmCode.isCode) {
+        return qdmCode;
+      }
       return new cql.Code(qdmCode.code, qdmCode.system, qdmCode.version, qdmCode.display);
     }
     return null;
