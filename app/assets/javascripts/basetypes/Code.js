15029fc479787abbe23b44418deecf6a7b984141
Matt Mayer <mmayer@mitre.org>
2019-06-27 09:06:27 -0400

    Address PR comments, add code casting tests

--- app/assets/javascripts/basetypes/Code.js
+++ app/assets/javascripts/basetypes/Code.js
@@ -8,16 +8,13 @@ Code.prototype = Object.create(mongoose.SchemaType.prototype);
 
 Code.prototype.cast = (code) => {
   if (code != null) {
+    // return code if it doesn't even need casting
+    if (code.isCode) {
+      return code;
+    }
     // handles codes that have not yet been cast to a code and those that have already been cast to a code
     if (code.code && code.system) {
-      if (typeof code.code === 'undefined') {
-        throw new Error(`Code: ${code} does not have a code`);
-      } else if (typeof code.system === 'undefined' && typeof code.system === 'undefined') {
-        throw new Error(`Code: ${code} does not have a system oid`);
-      }
-
       const val = { code: code.code, system: code.system };
-
       val.display = (typeof code.display !== 'undefined') ? code.display : null;
       val.version = (typeof code.version !== 'undefined') ? code.version : null;
 
