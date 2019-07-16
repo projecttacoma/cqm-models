31b97ca588c0dd4dda3fd97f393b6a03f3fdcb69
Luke Osborne <lwosborne@mitre.org>
2019-07-16 12:42:41 -0400

    Change QDMDate casting

--- app/assets/javascripts/basetypes/QDMDate.js
+++ app/assets/javascripts/basetypes/QDMDate.js
@@ -7,10 +7,29 @@ function QDMDate(key, options) {
 QDMDate.prototype = Object.create(mongoose.SchemaType.prototype);
 
 QDMDate.prototype.cast = (date) => {
-  if (!Date.parse(date)) {
-    throw new Error(`Date: ${date} is not a valid Date`);
+  if (date == null) {
+    return date;
   }
-  return cql.Date.fromJSDate(new Date(date));
+
+  // Already a CQL Date
+  if (date.isDate) {
+    return date;
+  }
+
+  // Object
+  if (typeof date === 'object') {
+    const keys = Object.keys(date);
+    if (keys.includes('year') && keys.includes('month') && keys.includes('day')) {
+      return new cql.Date(date.year, date.month, date.day);
+    }
+  }
+
+  // Date String
+  if (!cql.Date.parse(date)) {
+    throw new Error(`Date: ${date} is not a valid Date`);
+  } else {
+    return cql.Date.parse(date);
+  }
 };
 
 mongoose.Schema.Types.QDMDate = QDMDate;
