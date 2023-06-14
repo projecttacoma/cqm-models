3fcae27769f6a68b233920275d0f827392234573
adongare <ashok.dongare@semanticbits.com>
2023-06-14 10:49:18 -0400

    generate index and browser js for is operator support bump version

--- dist/index.js
+++ dist/index.js
@@ -3084,6 +3084,23 @@ function ComponentSchemaFunction(add, options) {
     extended.add(add);
   }
 
+  /* eslint no-underscore-dangle: 0 */
+  extended.methods._is = function _is(typeSpecifier) {
+    return this._typeHierarchy().some(
+      t => t.type === typeSpecifier.type && t.name === typeSpecifier.name
+    );
+  };
+
+  extended.methods._typeHierarchy = function _typeHierarchy() {
+    const typeName = this._type.replace(/QDM::/, '');
+    const ver = this.qdmVersion.replace('.', '_');
+    return [
+      {
+        name: `{urn:healthit-gov:qdm:v${ver}}${typeName}`,
+        type: 'NamedTypeSpecifier',
+      },
+    ];
+  };
   return extended;
 }
 
@@ -3180,6 +3197,23 @@ function EntitySchemaFunction(add, options) {
     extended.add(add);
   }
 
+  /* eslint no-underscore-dangle: 0 */
+  extended.methods._is = function _is(typeSpecifier) {
+    return this._typeHierarchy().some(
+      t => t.type === typeSpecifier.type && t.name === typeSpecifier.name
+    );
+  };
+
+  extended.methods._typeHierarchy = function _typeHierarchy() {
+    const typeName = this._type.replace(/QDM::/, '');
+    const ver = this.qdmVersion.replace('.', '_');
+    return [
+      {
+        name: `{urn:healthit-gov:qdm:v${ver}}${typeName}`,
+        type: 'NamedTypeSpecifier',
+      },
+    ];
+  };
   return extended;
 }
 
