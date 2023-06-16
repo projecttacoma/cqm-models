1f4662a3f1f67a8b9add7d86f033c64a1a48e1c2
adongare <ashok.dongare@semanticbits.com>
2023-06-16 13:59:55 -0400

    migrate jasmine/nyc to jest to fix security vulnerabilities

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+/*
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+module.exports = {
+  name: 'jest',
+  testEnvironment: 'node',
+  verbose: true,
+  roots: ['<rootDir>/app/assets/', '<rootDir>/spec/javascript'],
+  testMatch: ['<rootDir>//spec/javascript/**/*.js'],
+  modulePathIgnorePatterns: ['<rootDir>/dist/'],
+  collectCoverageFrom: [
+    'app/assets/**/*.js',
+    '!dist/*.js',
+    '!**/node_modules/**',
+  ],
+};
