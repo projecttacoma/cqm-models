823e31014a751ea2765cddeabc4082c0cfdd5fc6
Joseph Kotanchik <joseph.kotanchik@semanticbits.com>
2024-02-20 16:05:56 -0500

    Pass facility_location as a Hash to resolve unit test failure.
    Part of facility_loc hash commit

--- spec/cqm/models_spec.rb
+++ spec/cqm/models_spec.rb
@@ -168,8 +168,8 @@ RSpec.describe QDM do
     expect(@patient_de1.qdmPatient.diagnostic_studies.first.relevantPeriod.high.utc.to_s).to include('07:00:00')
 
     # DiatnosticStudyPerformed facilityLocation high and low should be two hours ahead
-    expect(@patient_de1.qdmPatient.diagnostic_studies.first.facilityLocation['locationPeriod'][:low].utc.to_s).to include('06:00:00')
-    expect(@patient_de1.qdmPatient.diagnostic_studies.first.facilityLocation['locationPeriod'][:high].utc.to_s).to include('07:00:00')
+    expect(@patient_de1.qdmPatient.diagnostic_studies.first.facilityLocation.locationPeriod.low.utc.to_s).to include('06:00:00')
+    expect(@patient_de1.qdmPatient.diagnostic_studies.first.facilityLocation.locationPeriod.high.utc.to_s).to include('07:00:00')
   end
 
   it 'shift patient data elements backwards in time' do
@@ -198,8 +198,8 @@ RSpec.describe QDM do
     expect(@patient_de2.qdmPatient.diagnostic_studies.first.relevantPeriod.high.utc.to_s).to include('03:00:00')
 
     # DiatnosticStudyPerformed facilityLocation high and low should be two hours behind
-    expect(@patient_de2.qdmPatient.diagnostic_studies.first.facilityLocation['locationPeriod'][:low].utc.to_s).to include('02:00:00')
-    expect(@patient_de2.qdmPatient.diagnostic_studies.first.facilityLocation['locationPeriod'][:high].utc.to_s).to include('03:00:00')
+    expect(@patient_de2.qdmPatient.diagnostic_studies.first.facilityLocation.locationPeriod.low.utc.to_s).to include('02:00:00')
+    expect(@patient_de2.qdmPatient.diagnostic_studies.first.facilityLocation.locationPeriod.high.utc.to_s).to include('03:00:00')
   end
 
   it 'relatedTo properly links data elements' do
